ea306dd8ae07afd7cb5cc1dc67507459523b48be
moksudahmed <k.moksud@gmail.com>
2023-02-01 13:31:51 +0600

    Web Scrapper Files added

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ import processdata
 import scraper_engine
 import log
 from time import sleep
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def hello_world():
